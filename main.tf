eb2f5e4df736ad6a8fc5795c8f66a7ceedc65698
vijay-project <vijaypatelworld@gmail.com>
2021-09-10 17:32:43 +0530

    six

--- main.tf
+++ main.tf
@@ -7,6 +7,7 @@ resource "azurerm_analysis_services_server" "default" {
   querypool_connection_mode = var.querypool_connection_mode
   backup_blob_container_uri = var.backup_blob_container_uri
   enable_power_bi_service   = var.enable_power_bi_service
+ 
   ipv4_firewall_rule {
     crl_name    = var.crl_name
     range_start = var.range_start
